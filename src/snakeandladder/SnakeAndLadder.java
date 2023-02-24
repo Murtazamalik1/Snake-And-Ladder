da0860d75b1b7c834cd011bfd5db1cb75fadd8be
murtazamalik <murtazamalik1319@gmail.com>
2023-02-24 17:13:29 +0530

    Exact100

--- src/snakeandladder/SnakeAndLadder.java
+++ src/snakeandladder/SnakeAndLadder.java
@@ -6,19 +6,25 @@ public class SnakeAndLadder {
     static final int IF_SNAKE = 2;
     static final int NO_PLAY = 0;
     static int currentPosition = 0;
+
     public static void main(String[] args) {
-        int dieNumber = (int) (Math.random() * 10) % 6+1;
-        int option = (int) (Math.random() * 10) % 3;
-        if (option == IF_LADDER){
-            currentPosition += dieNumber;
-        } else if (option == IF_SNAKE) {
-            currentPosition -= dieNumber;
-        } else if (option == NO_PLAY) {
-            System.out.println("No Play :");
+        while (currentPosition < 100) {
+            int dieNumber = (int) (Math.random() * 10) % 6 + 1;
+            int option = (int) (Math.random() * 10) % 3;
+            if (option == IF_LADDER) {
+                currentPosition += dieNumber;
+            } else if (option == IF_SNAKE) {
+                currentPosition -= dieNumber;
+            } else if (option == NO_PLAY) {
+                System.out.println("No Play :");
+            }
+            if (currentPosition < START_POSITION) {
+                currentPosition = 0;
+            }
+            if (currentPosition > 100){
+                currentPosition -= dieNumber;
+            }
+            System.out.println(currentPosition);
         }
-        if (currentPosition < START_POSITION){
-            currentPosition = 0;
-        }
-        System.out.println(currentPosition);
     }
 }
