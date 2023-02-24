790638e3d8eb7bb211b6d35054416278ffea67f4
murtazamalik <murtazamalik1319@gmail.com>
2023-02-24 14:51:28 +0530

    Added Start Position

--- src/snakeandladder/SnakeAndLadder.java
+++ src/snakeandladder/SnakeAndLadder.java
@@ -1,7 +1,8 @@
 package snakeandladder;
 
 public class SnakeAndLadder {
+    static final int START_POSITION = 0;
     public static void main(String[] args) {
-        System.out.println("WelCome To Snake And Ladder");
+        System.out.println("Current Position :" + START_POSITION);
     }
 }
