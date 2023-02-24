ca1c96d97b296931895d3710ef44ae44e458ef78
murtazamalik <murtazamalik1319@gmail.com>
2023-02-24 16:19:22 +0530

    Added Roll Dice

--- src/snakeandladder/SnakeAndLadder.java
+++ src/snakeandladder/SnakeAndLadder.java
@@ -3,6 +3,8 @@ package snakeandladder;
 public class SnakeAndLadder {
     static final int START_POSITION = 0;
     public static void main(String[] args) {
-        System.out.println("Current Position :" + START_POSITION);
+        System.out.println("WelCome To Snake And Ladder");
+        int dieNumber = (int) (Math.random() * 10) % 6+1;
+        System.out.println(dieNumber);
     }
 }
